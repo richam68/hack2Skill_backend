787cbf64eb39fd00b0593632f0f429ff4ec5fbbb
richam68 <richamhshwr2@gmail.com>
2025-07-24 07:25:53 +0530

    added validation

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 // Subtask Schema
 const subtaskSchema = new mongoose.Schema({
@@ -57,16 +58,40 @@ const userSchema = new mongoose.Schema(
     username: {
       type: String,
       required: [true, "User name is required"],
+      trim: true,
+      validate: {
+        validator: function (value) {
+          return /^[a-zA-Z\s]+$/.test(value); // allows only letters and spaces
+        },
+        message: "Username should contain only alphabets and spaces",
+      },
     },
     email: {
       type: String,
       required: [true, "Email is required"],
       unique: true,
+      lowercase: true,
+      validate: {
+        validator: (value) => validator.isEmail(value),
+        message: "Please enter a valid email address",
+      },
     },
     password: {
       type: String,
       required: [true, "Password is required"],
-      minlength: 6,
+      minlength: [6, "Password must be at least 6 characters long."],
+      // validate: {
+      //   validator: function (value) {
+      //     if (!/[a-zA-Z]/.test(value)) {
+      //       throw new Error("Password must contain at least one letter");
+      //     }
+      //     if (!/\d/.test(value)) {
+      //       throw new Error("Password must contain at least one number");
+      //     }
+      //     return true;
+      //   },
+      //   message: "Password validation failed",
+      // },
     },
     tasks: [taskSchema],
   },
